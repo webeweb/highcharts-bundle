de243cb1eb643633b2848803385dda3a1bcd7011
Camille A. <support@webeweb.fr>
2017-10-04 10:02:31 +0200

    Add bundle class

---
+++ WBWHighchartsBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the WBWHighchartsBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\HighchartsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Highcharts bundle.
+ *
+ * @author WBW <https://github.com/webeweb/WBWHighchartsBundle>
+ * @package WBW\HighchartsBundle
+ * @version 5.0.14
+ * @final
+ */
+final class WBWHighchartsBundle extends Bundle {
+
+}
