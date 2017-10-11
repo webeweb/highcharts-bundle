f61c892482dba5c78f4d6807e07db67b4e312015
webeweb <support@webeweb.fr>
2017-10-11 14:29:37 +0200

    Replace __DIR__ by $_SERVER["PWD"] (SensioLabs)

--- Tests/Twig/Extension/HighchartsTwigExtensionTest.php
+++ Tests/Twig/Extension/HighchartsTwigExtensionTest.php
@@ -33,7 +33,7 @@ final class HighchartsTwigExtensionTest extends PHPUnit_Framework_TestCase {
 	 */
 	public function testHighchartsChartFunction() {
 
-		$obj = new HighchartsTwigExtension(__DIR__ . "/../../../", "");
+		$obj = new HighchartsTwigExtension($_SERVER["PWD"], "");
 
 		$cht = new HighchartsChart();
 		$cht->newTitle()->setText("title");
@@ -47,7 +47,7 @@ final class HighchartsTwigExtensionTest extends PHPUnit_Framework_TestCase {
 	 */
 	public function testHighchartsScriptFunction() {
 
-		$obj = new HighchartsTwigExtension(__DIR__ . "/../../../", "");
+		$obj = new HighchartsTwigExtension($_SERVER["PWD"], "");
 
 		try {
 			$obj->highchartsScriptFunction("inexistant-script");
@@ -77,7 +77,7 @@ final class HighchartsTwigExtensionTest extends PHPUnit_Framework_TestCase {
 	 */
 	public function testHighchartsSetOptionsFunction() {
 
-		$obj = new HighchartsTwigExtension(__DIR__ . "/../../../", "");
+		$obj = new HighchartsTwigExtension($_SERVER["PWD"], "");
 
 		$opt = new HighchartsOptions();
 		$opt->newGlobal();
