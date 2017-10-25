f86ca43e6be2d01242d3f12355e9cb2d1ae8f593
webeweb <support@webeweb.fr>
2017-10-25 09:15:21 +0200

    Integrate Core Library

--- Tests/Twig/Extension/HighchartsTwigExtensionTest.php
+++ Tests/Twig/Extension/HighchartsTwigExtensionTest.php
@@ -17,119 +17,118 @@ use Twig_Node;
 use Twig_SimpleFunction;
 use WBW\Bundle\HighchartsBundle\API\HighchartsChart;
 use WBW\Bundle\HighchartsBundle\API\HighchartsOptions;
-use WBW\Bundle\HighchartsBundle\Exception\HighchartsFileNotFoundException;
 use WBW\Bundle\HighchartsBundle\Twig\Extension\HighchartsTwigExtension;
+use WBW\Library\Core\Exception\File\FileNotFoundException;
 
 /**
  * Highcharts Twig extension test.
  *
  * @author NdC/WBW <https://github.com/webeweb/>
  * @package WBW\Bundle\HighchartsBundle\Tests\Wrapper
- * @version 5.0.14
  * @final
  */
 final class HighchartsTwigExtensionTest extends PHPUnit_Framework_TestCase {
 
-	/**
-	 * Tests the getFunctions() method.
-	 *
-	 * @return void
-	 */
-	public function testGetFunctions() {
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
 
-		$obj = new HighchartsTwigExtension(getcwd(), "");
+        $obj = new HighchartsTwigExtension(getcwd(), "");
 
-		$this->assertCount(3, $obj->getFunctions(), "The method getFunctions() does not return the expected count");
+        $this->assertCount(3, $obj->getFunctions(), "The method getFunctions() does not return the expected count");
 
-		$this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[0], "The method getFunctions() does not return the expected object on item 0");
-		$this->assertEquals("highchartsChart", $obj->getFunctions()[0]->getName(), "The method getName() does not return the expected name on item 0");
-		$this->assertEquals([$obj, "highchartsChartFunction"], $obj->getFunctions()[0]->getCallable(), "The method getCallable() does not return the expected callable on item 0");
-		$this->assertEquals(["html"], $obj->getFunctions()[0]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 0");
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[0], "The method getFunctions() does not return the expected object on item 0");
+        $this->assertEquals("highchartsChart", $obj->getFunctions()[0]->getName(), "The method getName() does not return the expected name on item 0");
+        $this->assertEquals([$obj, "highchartsChartFunction"], $obj->getFunctions()[0]->getCallable(), "The method getCallable() does not return the expected callable on item 0");
+        $this->assertEquals(["html"], $obj->getFunctions()[0]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 0");
 
-		$this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[1], "The method getFunctions() does not return the expected object on item 1");
-		$this->assertEquals("highchartsScript", $obj->getFunctions()[1]->getName(), "The method getName() does not return the expected name on item 1");
-		$this->assertEquals([$obj, "highchartsScriptFunction"], $obj->getFunctions()[1]->getCallable(), "The method getCallable() does not return the expected callable on item 1");
-		$this->assertEquals(["html"], $obj->getFunctions()[1]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 1");
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[1], "The method getFunctions() does not return the expected object on item 1");
+        $this->assertEquals("highchartsScript", $obj->getFunctions()[1]->getName(), "The method getName() does not return the expected name on item 1");
+        $this->assertEquals([$obj, "highchartsScriptFunction"], $obj->getFunctions()[1]->getCallable(), "The method getCallable() does not return the expected callable on item 1");
+        $this->assertEquals(["html"], $obj->getFunctions()[1]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 1");
 
-		$this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[2], "The method getFunctions() does not return the expected object on item 2");
-		$this->assertEquals("highchartsSetOptions", $obj->getFunctions()[2]->getName(), "The method getName() does not return the expected name on item 2");
-		$this->assertEquals([$obj, "highchartsSetOptionsFunction"], $obj->getFunctions()[2]->getCallable(), "The method getCallable() does not return the expected callable on item 2");
-		$this->assertEquals(["html"], $obj->getFunctions()[2]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 2");
-	}
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $obj->getFunctions()[2], "The method getFunctions() does not return the expected object on item 2");
+        $this->assertEquals("highchartsSetOptions", $obj->getFunctions()[2]->getName(), "The method getName() does not return the expected name on item 2");
+        $this->assertEquals([$obj, "highchartsSetOptionsFunction"], $obj->getFunctions()[2]->getCallable(), "The method getCallable() does not return the expected callable on item 2");
+        $this->assertEquals(["html"], $obj->getFunctions()[2]->getSafe(new Twig_Node()), "The method getSafe() does not return the expected safe on item 2");
+    }
 
-	/**
-	 * Tests the highchartsChartFunction() method.
-	 *
-	 * @return void
-	 */
-	public function testHighchartsChartFunction() {
+    /**
+     * Tests the highchartsChartFunction() method.
+     *
+     * @return void
+     */
+    public function testHighchartsChartFunction() {
 
-		$cht = new HighchartsChart();
-		$cht->newTitle()->setText("title");
+        $cht = new HighchartsChart();
+        $cht->newTitle()->setText("title");
 
-		$obj1 = new HighchartsTwigExtension(getcwd(), "");
+        $obj1 = new HighchartsTwigExtension(getcwd(), "");
 
-		$res1 = "<script type=\"text/javascript\">\nHighcharts.chart('selector', {\"title\":{\"text\":\"title\"}});\n</script>";
-		$this->assertEquals($res1, $obj1->highchartsChartFunction("selector", $cht), "The method highchartsChartFunction() does not return the expected string in prod");
+        $res1 = "<script type=\"text/javascript\">\nHighcharts.chart('selector', {\"title\":{\"text\":\"title\"}});\n</script>";
+        $this->assertEquals($res1, $obj1->highchartsChartFunction("selector", $cht), "The method highchartsChartFunction() does not return the expected string in prod");
 
-		$obj2 = new HighchartsTwigExtension(getcwd(), "dev");
+        $obj2 = new HighchartsTwigExtension(getcwd(), "dev");
 
-		$res2 = "<script type=\"text/javascript\">\nHighcharts.chart('selector', {\n    \"title\": {\n        \"text\": \"title\"\n    }\n});\n</script>";
-		$this->assertEquals($res2, $obj2->highchartsChartFunction("selector", $cht), "The method highchartsChartFunction() does not return the expected string in dev");
-	}
+        $res2 = "<script type=\"text/javascript\">\nHighcharts.chart('selector', {\n    \"title\": {\n        \"text\": \"title\"\n    }\n});\n</script>";
+        $this->assertEquals($res2, $obj2->highchartsChartFunction("selector", $cht), "The method highchartsChartFunction() does not return the expected string in dev");
+    }
 
-	/**
-	 * Tests the highchartsScriptFunction() method.
-	 *
-	 * @return void
-	 */
-	public function testHighchartsScriptFunction() {
+    /**
+     * Tests the highchartsScriptFunction() method.
+     *
+     * @return void
+     */
+    public function testHighchartsScriptFunction() {
 
-		$obj = new HighchartsTwigExtension(getcwd(), "");
+        $obj = new HighchartsTwigExtension(getcwd(), "");
 
-		try {
-			$obj->highchartsScriptFunction("inexistant-script");
-		} catch (Exception $ex) {
-			$this->assertInstanceOf(HighchartsFileNotFoundException::class, $ex, "The method highchartsScript() does not throw the expected exception");
-			$this->assertEquals("The file \"code/inexistant-script.js\" was not found", $ex->getMessage(), "The method getMessage() does not return the expected string");
-		}
+        try {
+            $obj->highchartsScriptFunction("inexistant-script");
+        } catch (Exception $ex) {
+            $this->assertInstanceOf(FileNotFoundException::class, $ex, "The method highchartsScript() does not throw the expected exception");
+            $this->assertEquals("The file \"code/inexistant-script.js\" is not found", $ex->getMessage(), "The method getMessage() does not return the expected string");
+        }
 
-		$res1 = "<script src=\"/bundles/wbwhighcharts/code/highcharts.js\" type=\"text/javascript\"></script>";
-		$this->assertEquals($res1, $obj->highchartsScriptFunction("highcharts"), "The method highchartsScriptFunction() does not return the expected string");
+        $res1 = "<script src=\"/bundles/wbwhighcharts/code/highcharts.js\" type=\"text/javascript\"></script>";
+        $this->assertEquals($res1, $obj->highchartsScriptFunction("highcharts"), "The method highchartsScriptFunction() does not return the expected string");
 
-		$res2 = "<script src=\"/bundles/wbwhighcharts/code/modules/exporting.js\" type=\"text/javascript\"></script>";
-		$this->assertEquals($res2, $obj->highchartsScriptFunction("modules/exporting"), "The method highchartsScriptFunction() does not return the expected string");
+        $res2 = "<script src=\"/bundles/wbwhighcharts/code/modules/exporting.js\" type=\"text/javascript\"></script>";
+        $this->assertEquals($res2, $obj->highchartsScriptFunction("modules/exporting"), "The method highchartsScriptFunction() does not return the expected string");
 
-		$res2_1 = "<script src=\"/bundles/wbwhighcharts/code/modules/exporting.js\" type=\"text/javascript\"></script>";
-		$this->assertEquals($res2_1, $obj->highchartsScriptFunction("exporting", "code/modules"), "The method highchartsScriptFunction() does not return the expected string");
+        $res2_1 = "<script src=\"/bundles/wbwhighcharts/code/modules/exporting.js\" type=\"text/javascript\"></script>";
+        $this->assertEquals($res2_1, $obj->highchartsScriptFunction("exporting", "code/modules"), "The method highchartsScriptFunction() does not return the expected string");
 
-		$res3 = "<script src=\"/bundles/wbwhighcharts/code/themes/dark-unica.js\" type=\"text/javascript\"></script>";
-		$this->assertEquals($res3, $obj->highchartsScriptFunction("themes/dark-unica"), "The method highchartsScriptFunction() does not return the expected string");
+        $res3 = "<script src=\"/bundles/wbwhighcharts/code/themes/dark-unica.js\" type=\"text/javascript\"></script>";
+        $this->assertEquals($res3, $obj->highchartsScriptFunction("themes/dark-unica"), "The method highchartsScriptFunction() does not return the expected string");
 
-		$res3_1 = "<script src=\"/bundles/wbwhighcharts/code/themes/dark-unica.js\" type=\"text/javascript\"></script>";
-		$this->assertEquals($res3_1, $obj->highchartsScriptFunction("dark-unica", "code/themes"), "The method highchartsScriptFunction() does not return the expected string");
-	}
+        $res3_1 = "<script src=\"/bundles/wbwhighcharts/code/themes/dark-unica.js\" type=\"text/javascript\"></script>";
+        $this->assertEquals($res3_1, $obj->highchartsScriptFunction("dark-unica", "code/themes"), "The method highchartsScriptFunction() does not return the expected string");
+    }
 
-	/**
-	 * Tests the highchartsSetOptionsFunction() method.
-	 *
-	 * @return void
-	 */
-	public function testHighchartsSetOptionsFunction() {
+    /**
+     * Tests the highchartsSetOptionsFunction() method.
+     *
+     * @return void
+     */
+    public function testHighchartsSetOptionsFunction() {
 
-		$opt = new HighchartsOptions();
-		$opt->newGlobal();
-		$opt->newLang();
+        $opt = new HighchartsOptions();
+        $opt->newGlobal();
+        $opt->newLang();
 
-		$obj1 = new HighchartsTwigExtension(getcwd(), "");
+        $obj1 = new HighchartsTwigExtension(getcwd(), "");
 
-		$res1 = "<script type=\"text/javascript\">\nHighcharts.setOptions({\"global\":[],\"lang\":[]});\n</script>";
-		$this->assertEquals($res1, $obj1->highchartsSetOptionsFunction($opt), "The method highchartsSetOptionsFunction() does not return the expected string in prod");
+        $res1 = "<script type=\"text/javascript\">\nHighcharts.setOptions({\"global\":[],\"lang\":[]});\n</script>";
+        $this->assertEquals($res1, $obj1->highchartsSetOptionsFunction($opt), "The method highchartsSetOptionsFunction() does not return the expected string in prod");
 
-		$obj2 = new HighchartsTwigExtension(getcwd(), "dev");
+        $obj2 = new HighchartsTwigExtension(getcwd(), "dev");
 
-		$res2 = "<script type=\"text/javascript\">\nHighcharts.setOptions({\n    \"global\": [],\n    \"lang\": []\n});\n</script>";
-		$this->assertEquals($res2, $obj2->highchartsSetOptionsFunction($opt), "The method highchartsSetOptionsFunction() does not return the expected string in dev");
-	}
+        $res2 = "<script type=\"text/javascript\">\nHighcharts.setOptions({\n    \"global\": [],\n    \"lang\": []\n});\n</script>";
+        $this->assertEquals($res2, $obj2->highchartsSetOptionsFunction($opt), "The method highchartsSetOptionsFunction() does not return the expected string in dev");
+    }
 
 }
--- Twig/Extension/HighchartsTwigExtension.php
+++ Twig/Extension/HighchartsTwigExtension.php
@@ -15,158 +15,157 @@ use Twig_Extension;
 use Twig_SimpleFunction;
 use WBW\Bundle\HighchartsBundle\API\HighchartsChart;
 use WBW\Bundle\HighchartsBundle\API\HighchartsOptions;
-use WBW\Bundle\HighchartsBundle\Exception\HighchartsFileNotFoundException;
 use WBW\Bundle\HighchartsBundle\Wrapper\HighchartsWrapper;
+use WBW\Library\Core\Exception\File\FileNotFoundException;
 
 /**
  * Highcharts Twig extension.
  *
  * @author NdC/WBW <https://github.com/webeweb/>
  * @package WBW\Bundle\HighchartsBundle\Twig\Extension
- * @version 5.0.14
  * @final
  */
 final class HighchartsTwigExtension extends Twig_Extension {
 
-	/**
-	 * Service name.
-	 */
-	const SERVICE_NAME = "webeweb.highcharts-bundle.twig.extension.highcharts";
+    /**
+     * Service name.
+     */
+    const SERVICE_NAME = "webeweb.highcharts-bundle.twig.extension.highcharts";
 
-	/**
-	 * Directory.
-	 *
-	 * @var string
-	 */
-	private $directory;
+    /**
+     * Directory.
+     *
+     * @var string
+     */
+    private $directory;
 
-	/**
-	 * Environment.
-	 *
-	 * @var string
-	 */
-	private $environment;
+    /**
+     * Environment.
+     *
+     * @var string
+     */
+    private $environment;
 
-	/**
-	 * Wrapper.
-	 *
-	 * @var HightchartsWrapper
-	 */
-	private $wrapper;
+    /**
+     * Wrapper.
+     *
+     * @var HightchartsWrapper
+     */
+    private $wrapper;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param string $directory The directory.
-	 * @param string $environment The environment.
-	 */
-	public final function __construct($directory, $environment) {
-		$this->directory	 = $directory;
-		$this->environment	 = $environment;
-		$this->wrapper		 = new HighchartsWrapper();
-	}
+    /**
+     * Constructor.
+     *
+     * @param string $directory The directory.
+     * @param string $environment The environment.
+     */
+    public final function __construct($directory, $environment) {
+        $this->directory   = $directory;
+        $this->environment = $environment;
+        $this->wrapper     = new HighchartsWrapper();
+    }
 
-	/**
-	 * Get the Twig functions.
-	 *
-	 * @return array Returns the Twig functions.
-	 */
-	public function getFunctions() {
-		return [
-			new Twig_SimpleFunction('highchartsChart', [$this, 'highchartsChartFunction'], ['is_safe' => ['html']]),
-			new Twig_SimpleFunction('highchartsScript', [$this, 'highchartsScriptFunction'], ['is_safe' => ['html']]),
-			new Twig_SimpleFunction('highchartsSetOptions', [$this, 'highchartsSetOptionsFunction'], ['is_safe' => ['html']]),
-		];
-	}
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction('highchartsChart', [$this, 'highchartsChartFunction'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('highchartsScript', [$this, 'highchartsScriptFunction'], ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('highchartsSetOptions', [$this, 'highchartsSetOptionsFunction'], ['is_safe' => ['html']]),
+        ];
+    }
 
-	/**
-	 * Get the resources directory.
-	 *
-	 * @return string Returns the resources directory.
-	 */
-	private function getResourcesDirectory() {
-		return $this->directory . "/Resources";
-	}
+    /**
+     * Get the resources directory.
+     *
+     * @return string Returns the resources directory.
+     */
+    private function getResourcesDirectory() {
+        return $this->directory . "/Resources";
+    }
 
-	/**
-	 * Displays an Highcharts.chart().
-	 *
-	 * @param string $selector The selector.
-	 * @param HighchartsChart $chart The chart.
-	 * @return string Returns the Highcharts.chart().
-	 */
-	public function highchartsChartFunction($selector, HighchartsChart $chart) {
+    /**
+     * Displays an Highcharts.chart().
+     *
+     * @param string $selector The selector.
+     * @param HighchartsChart $chart The chart.
+     * @return string Returns the Highcharts.chart().
+     */
+    public function highchartsChartFunction($selector, HighchartsChart $chart) {
 
-		// Initialize the output.
-		$output		 = [];
-		$output[]	 = "<script type=\"text/javascript\">\n";
-		$output[]	 = "Highcharts.chart('" . $selector . "', ";
-		if ($this->isDevEnvironment()) {
-			$output[] = $this->wrapper->unwrap(json_encode($chart, JSON_PRETTY_PRINT));
-		} else {
-			$output[] = $this->wrapper->unwrap(json_encode($chart));
-		}
-		$output[]	 = ");\n";
-		$output[]	 = "</script>";
+        // Initialize the output.
+        $output   = [];
+        $output[] = "<script type=\"text/javascript\">\n";
+        $output[] = "Highcharts.chart('" . $selector . "', ";
+        if ($this->isDevEnvironment()) {
+            $output[] = $this->wrapper->unwrap(json_encode($chart, JSON_PRETTY_PRINT));
+        } else {
+            $output[] = $this->wrapper->unwrap(json_encode($chart));
+        }
+        $output[] = ");\n";
+        $output[] = "</script>";
 
-		// Return the output.
-		return implode("", $output);
-	}
+        // Return the output.
+        return implode("", $output);
+    }
 
-	/**
-	 * Displays an Highcharts script.
-	 *
-	 * @param string $script The scriptname.
-	 * @param string $subdirectory The sub directory.
-	 * @return string Returns the Highcharts script.
-	 * @throws HighchartsFileNotFoundException Throws an Highcharts file not found exception if the script is not found.
-	 */
-	public function highchartsScriptFunction($script, $subdirectory = "code") {
+    /**
+     * Displays an Highcharts script.
+     *
+     * @param string $script The scriptname.
+     * @param string $subdirectory The sub directory.
+     * @return string Returns the Highcharts script.
+     * @throws FileNotFoundException Throws a file not found exception if the script is not found.
+     */
+    public function highchartsScriptFunction($script, $subdirectory = "code") {
 
-		// Initialize the filename.
-		$filename = implode("/", [$subdirectory, $script . ".js"]);
+        // Initialize the filename.
+        $filename = implode("/", [$subdirectory, $script . ".js"]);
 
-		// Initialize and check the filepath.
-		$filepath = $this->getResourcesDirectory() . "/public/" . $filename;
-		if (!file_exists($filepath)) {
-			throw new HighchartsFileNotFoundException($filename);
-		}
+        // Initialize and check the filepath.
+        $filepath = $this->getResourcesDirectory() . "/public/" . $filename;
+        if (!file_exists($filepath)) {
+            throw new FileNotFoundException($filename);
+        }
 
-		// Return the output.
-		return "<script src=\"/bundles/wbwhighcharts/" . $filename . "\" type=\"text/javascript\"></script>";
-	}
+        // Return the output.
+        return "<script src=\"/bundles/wbwhighcharts/" . $filename . "\" type=\"text/javascript\"></script>";
+    }
 
-	/**
-	 * Displays an Highcharts.setOptions()
-	 *
-	 * @param HighchartsOptions $options The options.
-	 * @return string Returns the Highcharts.setOptions().
-	 */
-	public function highchartsSetOptionsFunction(HighchartsOptions $options) {
+    /**
+     * Displays an Highcharts.setOptions()
+     *
+     * @param HighchartsOptions $options The options.
+     * @return string Returns the Highcharts.setOptions().
+     */
+    public function highchartsSetOptionsFunction(HighchartsOptions $options) {
 
-		// Initialize the output.
-		$output		 = [];
-		$output[]	 = "<script type=\"text/javascript\">\n";
-		$output[]	 = "Highcharts.setOptions(";
-		if ($this->isDevEnvironment()) {
-			$output[] = $this->wrapper->unwrap(json_encode($options, JSON_PRETTY_PRINT));
-		} else {
-			$output[] = $this->wrapper->unwrap(json_encode($options));
-		}
-		$output[]	 = ");\n";
-		$output[]	 = "</script>";
+        // Initialize the output.
+        $output   = [];
+        $output[] = "<script type=\"text/javascript\">\n";
+        $output[] = "Highcharts.setOptions(";
+        if ($this->isDevEnvironment()) {
+            $output[] = $this->wrapper->unwrap(json_encode($options, JSON_PRETTY_PRINT));
+        } else {
+            $output[] = $this->wrapper->unwrap(json_encode($options));
+        }
+        $output[] = ");\n";
+        $output[] = "</script>";
 
-		// Return the output.
-		return implode("", $output);
-	}
+        // Return the output.
+        return implode("", $output);
+    }
 
-	/**
-	 * Determines if the environement is dev.
-	 *
-	 * @return boolean Returns true in case of success, false otherwise.
-	 */
-	private function isDevEnvironment() {
-		return $this->environment === "dev";
-	}
+    /**
+     * Determines if the environement is dev.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    private function isDevEnvironment() {
+        return $this->environment === "dev";
+    }
 
 }
